648a3f643a1b8e5d2945b009aadda76f39d23b23
“Yile <yile@ualberta.ca>
2023-03-10 16:02:53 -0700

    green:count test

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -52,7 +52,9 @@ public class CustomList extends ArrayAdapter<City> {
     public void addCity(City city){
         cities.add(city);
     }
-
+    public int countCity() {
+        return cities.size();
+    }
     public boolean hasCity(City city) {
         return cities.contains(city);
     }
