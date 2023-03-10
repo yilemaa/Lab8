f523331a9a91af82a9fa346e11b790831ca107ae
“Yile <yile@ualberta.ca>
2023-03-10 15:58:07 -0700

    green:delete test

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -59,6 +59,6 @@ public class CustomList extends ArrayAdapter<City> {
         return cities.contains(city);
     }
     public void deleteCity(City city) {
-
+        this.cities.remove(city);
     }
 }
